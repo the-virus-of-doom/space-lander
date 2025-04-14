fec05d53fa8564a9876f3444c84127673394df2e
Zach / Virus <thevirusofdoom1337@gmail.com>
2025-04-13 22:48:14 -0400

    fix loading screen alignment
    - add loading text for loading bar

--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -7,13 +7,22 @@ export class Preloader extends Scene {
 
     init() {
         //  We loaded this image in our Boot Scene, so we can display it here
-        this.add.image(512, 384, 'background');
+        this.add.image(400, 400, 'background');
+
+        this.add.text(300, 300, 'L o a d i n g . . . ', {
+            fontFamily: 'Arial Black',
+            fontSize: 24,
+            color: '#ffffff',
+            stroke: '#000000',
+            strokeThickness: 6,
+            align: 'center',
+        });
 
         //  A simple progress bar. This is the outline of the bar.
-        this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+        this.add.rectangle(400, 400, 468, 32).setStrokeStyle(1, 0xffffff);
 
         //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
+        const bar = this.add.rectangle(400 - 230, 400, 4, 28, 0xffffff);
 
         //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
         this.load.on('progress', (progress: number) => {

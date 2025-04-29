004dcd05e635b7797022dafc3fec457fbb31a436
Zach / Virus <thevirusofdoom1337@gmail.com>
2025-04-29 00:41:59 -0400

    add Zig-Zag level
    first you zig, then you zag

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -67,6 +67,50 @@ export class Game extends Scene {
                 },
             ],
         },
+        {
+            name: 'Zig-Zag',
+            background: {
+                x: 400,
+                y: 300,
+                assetName: 'NASA_background',
+                backgroundColor: 0xb46017,
+            },
+            startPlatform: {
+                x: 100,
+                y: 550,
+            },
+            endPlatform: {
+                x: 700,
+                y: 230,
+            },
+            fuelPickup: {
+                x: 600,
+                y: 375,
+                startAmount: 100,
+            },
+            ground: {
+                x: 400,
+                y: 575,
+                assetName: 'ground',
+            },
+            groundPlatforms: [
+                {
+                    x: 350,
+                    y: 220,
+                    assetName: 'ground',
+                },
+                {
+                    x: 200,
+                    y: 400,
+                    assetName: 'ground',
+                },
+                {
+                    x: 750,
+                    y: 250,
+                    assetName: 'ground',
+                },
+            ],
+        },
     ];
 
     constructor() {

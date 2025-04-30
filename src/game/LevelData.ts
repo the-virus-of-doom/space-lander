dad11b9375c08edcc18aaa4f924e39d8b5415b9e
Zach / Virus <thevirusofdoom1337@gmail.com>
2025-04-30 00:25:57 -0400

    move levels to dedicated file

---
+++ src/game/LevelData.ts
@@ -0,0 +1,92 @@
+import { Level } from './level';
+
+export const levelData: Level[] = [
+    {
+        name: 'Tutorial',
+        background: {
+            x: 400,
+            y: 300,
+            assetName: 'NASA_background',
+            backgroundColor: 0xb46017,
+        },
+        startPlatform: {
+            x: 100,
+            y: 500,
+        },
+        endPlatform: {
+            x: 700,
+            y: 200,
+        },
+        fuelPickup: {
+            x: 700,
+            y: 400,
+            startAmount: 100,
+        },
+        ground: {
+            x: 400,
+            y: 575,
+            assetName: 'ground',
+        },
+        groundPlatforms: [
+            {
+                x: 600,
+                y: 400,
+                assetName: 'ground',
+            },
+            {
+                x: 50,
+                y: 250,
+                assetName: 'ground',
+            },
+            {
+                x: 750,
+                y: 220,
+                assetName: 'ground',
+            },
+        ],
+    },
+    {
+        name: 'Zig-Zag',
+        background: {
+            x: 400,
+            y: 300,
+            assetName: 'NASA_background',
+            backgroundColor: 0xb46017,
+        },
+        startPlatform: {
+            x: 100,
+            y: 550,
+        },
+        endPlatform: {
+            x: 700,
+            y: 230,
+        },
+        fuelPickup: {
+            x: 600,
+            y: 375,
+            startAmount: 100,
+        },
+        ground: {
+            x: 400,
+            y: 575,
+            assetName: 'ground',
+        },
+        groundPlatforms: [
+            {
+                x: 350,
+                y: 220,
+                assetName: 'ground',
+            },
+            {
+                x: 200,
+                y: 400,
+                assetName: 'ground',
+            },
+            {
+                x: 750,
+                y: 250,
+                assetName: 'ground',
+            },
+        ],
+    },
+];
--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -1,6 +1,7 @@
 import { EventBus } from '../EventBus';
 import { Scene } from 'phaser';
 import { Level } from '../level';
+import { levelData } from '../LevelData';
 
 export class Game extends Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
@@ -23,96 +24,7 @@ export class Game extends Scene {
     currentLevel: number = 0;
     debug: boolean = false;
 
-    levels: Level[] = [
-        {
-            name: 'Tutorial',
-            background: {
-                x: 400,
-                y: 300,
-                assetName: 'NASA_background',
-                backgroundColor: 0xb46017,
-            },
-            startPlatform: {
-                x: 100,
-                y: 500,
-            },
-            endPlatform: {
-                x: 700,
-                y: 200,
-            },
-            fuelPickup: {
-                x: 700,
-                y: 400,
-                startAmount: 100,
-            },
-            ground: {
-                x: 400,
-                y: 575,
-                assetName: 'ground',
-            },
-            groundPlatforms: [
-                {
-                    x: 600,
-                    y: 400,
-                    assetName: 'ground',
-                },
-                {
-                    x: 50,
-                    y: 250,
-                    assetName: 'ground',
-                },
-                {
-                    x: 750,
-                    y: 220,
-                    assetName: 'ground',
-                },
-            ],
-        },
-        {
-            name: 'Zig-Zag',
-            background: {
-                x: 400,
-                y: 300,
-                assetName: 'NASA_background',
-                backgroundColor: 0xb46017,
-            },
-            startPlatform: {
-                x: 100,
-                y: 550,
-            },
-            endPlatform: {
-                x: 700,
-                y: 230,
-            },
-            fuelPickup: {
-                x: 600,
-                y: 375,
-                startAmount: 100,
-            },
-            ground: {
-                x: 400,
-                y: 575,
-                assetName: 'ground',
-            },
-            groundPlatforms: [
-                {
-                    x: 350,
-                    y: 220,
-                    assetName: 'ground',
-                },
-                {
-                    x: 200,
-                    y: 400,
-                    assetName: 'ground',
-                },
-                {
-                    x: 750,
-                    y: 250,
-                    assetName: 'ground',
-                },
-            ],
-        },
-    ];
+    levels: Level[] = levelData;
 
     constructor() {
         super('Game');

c7cb16411f6b6f74924e43c8599ca0699a6f7196
Zach / Virus <thevirusofdoom1337@gmail.com>
2025-04-30 20:53:43 -0400

    add validation to levels fetched over the network

--- src/game/NetworkLoader.ts
+++ src/game/NetworkLoader.ts
@@ -6,11 +6,54 @@ export class NetworkLoader {
         this.url = url;
     }
 
+    validateLevel(level: Level): boolean {
+        if (!level.name) {
+            return false;
+        }
+        if (
+            !level.background.x ||
+            !level.background.y ||
+            !level.background.assetName ||
+            !level.background.backgroundColor
+        ) {
+            return false;
+        }
+        if (!level.startPlatform.x || !level.startPlatform.y) {
+            return false;
+        }
+        if (!level.endPlatform.x || !level.endPlatform.y) {
+            return false;
+        }
+        if (
+            !level.fuelPickup.x ||
+            !level.fuelPickup.y ||
+            !level.fuelPickup.startAmount
+        ) {
+            return false;
+        }
+        if (!level.ground.x || !level.ground.y) {
+            return false;
+        }
+        if (!level.groundPlatforms) {
+            return false;
+        }
+        return true;
+    }
+
     async loadLevelData() {
-        let response = await fetch(this.url).then((response) => {
+        let response: Level[] = await fetch(this.url).then((response) => {
             return response.json();
         });
-        // TODO: add validation
-        return response as unknown as Level[];
+
+        let validatedLevels: Level[] = [];
+        response.forEach((level) => {
+            if (this.validateLevel(level)) {
+                validatedLevels.push(level);
+            } else {
+                console.warn('filtered invalid level');
+            }
+        });
+
+        return validatedLevels as unknown as Level[];
     }
 }
